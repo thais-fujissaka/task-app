2d1392e27197940e7300b788e061e881e0ac3eba
thais-fujissaka <80991615+thais-fujissaka@users.noreply.github.com>
2024-11-21 14:00:51 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -59,9 +59,26 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas: " + realizadas.length,
         choices: [...realizadas]
     })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return !meta.checked
+    })
+
+    if(abertas.length == 0){
+        console.log("Não existem metas abertas! c:")
+        return
+    }
+
+    await select({
+        message: "Metas Abertas: " + abertas.length,
+        choices: [...abertas]
+    })
+
 }
 
 const start = async () => {
@@ -84,6 +101,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -102,6 +123,9 @@ const start = async () => {
             case "realizadas":
                 await metasRealizadas()
                 break
+                case "abertas":
+                    await metasAbertas()
+                    break
             case "sair":
                 console.log("Até a próxima!")
                 return
